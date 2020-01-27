1d88f4d07bd68d0c3f7922d39e00b1b65457361b
Oscar Zapata <0northbridge.cpu1@gmail.com>
2020-01-27 14:40:08 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -6,8 +6,8 @@ def line(numinline)
  if numinline.length == 0 
    puts "The line is currently empty."
  else 
-  line_method_arr.each_with_index do |name,index|
-       line_method_arr.push("#{index}. #{name}")
+  numinline.each_with_index do |name,index|
+       line_method_arr.push("#{index + 1}. #{name}")
     end
     puts "The line is currently: #{line_method_arr.join(" ")}"
  end 
