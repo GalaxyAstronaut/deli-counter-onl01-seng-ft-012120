c3e4687c38fd6562e98988292cbaa57fcb9e3a8f
Oscar Zapata <0northbridge.cpu1@gmail.com>
2020-01-27 14:36:49 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,14 +1,15 @@
 # Write your code here.
 katz_deli = []
 
-def line(katz_deli)
-  
- if katz_deli.length == 0 
+def line(numinline)
+  line_method_arr = []
+ if numinline.length == 0 
    puts "The line is currently empty."
  else 
-   katz_deli.each_with_index do |name,index|
-       "The line is currently: #{index + 1}. #{name} "
+  line_method_arr.each.with_index(0) do |name,index|
+       line_method_arr.push("#{index}. #{name}")
     end
+    puts "The line is: #{line_method_arr.join(" ")}"
  end 
 
 end
