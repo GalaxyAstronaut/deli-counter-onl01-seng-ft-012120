202db6e2cb898e9b5ac7c1977ff9a2dcc96055d9
Oscar Zapata <0northbridge.cpu1@gmail.com>
2020-01-27 15:06:56 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -13,3 +13,11 @@ def line(numinline)
  end 
 
 end
+
+
+def take_a_number(katz_deli,name)
+  katz_deli.push(name)
+  puts "Welcome, #{name}. You are number #{katz_deli.length} in line."
+  
+  
+end
